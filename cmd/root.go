87052b0b9dde3edf76034430d547b2262120ff43
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-05-04 08:01:11 -0700

    trim of autogenerated docs.

--- cmd/root.go
+++ cmd/root.go
@@ -31,19 +31,10 @@ import (
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "mypass",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Golang pass complient CLI",
+	Long:  `CLI application aiming to replicate pass.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -56,16 +47,9 @@ func Execute() {
 }
 
 func init() {
+	fmt.Printf("I am the init().\n")
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mypass.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
