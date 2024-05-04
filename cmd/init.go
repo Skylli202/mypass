a74ace0ed504f856f637106e685c9b6a1d166ede
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-05-04 11:10:12 -0700

    feat: conform the init sub command to the original pass sub command.

--- cmd/init.go
+++ cmd/init.go
@@ -30,13 +30,13 @@ import (
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Initialize new password storage and use gpg-id for encryption. Selectively reencrypt existing passwords using new gpg-id.",
+	Long: `Initialize  new  password storage and use gpg-id for encryption. Multiple gpg-ids may be specified, in order to encrypt each password with multiple ids.
+This command must be run first before a password store can be used. If the specified gpg-id is different from the key used in any existing files,  these
+files  will  be  reencrypted to use the new id.  Note that use of gpg-agent(1) is recommended so that the batch decryption does not require as much user
+intervention. If --path or -p is specified, along with an argument, a specific gpg-id or set of gpg-ids is assigned for that specific sub folder of  the
+password  store. If only one gpg-id is given, and it is an empty string, then the current .gpg-id file for the specified sub-folder (or root if unspeci‐
+fied) is removed.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("init called")
 	},
@@ -44,14 +44,5 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(initCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	initCmd.Flags().StringP("path", "p", "", "Specific gpg-id, or set of gpg-ids, for specific sub-folder of the password store")
 }
